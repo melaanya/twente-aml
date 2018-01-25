c1d89752d8a4bbd7719b6f45002b1d5d0751affa
Ashwin Sadananda Bhat <a.sadanandabhat+sourcetree@student.utwente.nl>
2018-01-25 16:24:08 +0100

    sklearn and xgboost tuned code added

--- project/eda/spyder/boston_sklearn.py
+++ project/eda/spyder/boston_sklearn.py
@@ -23,28 +23,28 @@ from util import plot_top_features
 train = pd.read_csv('../data/Boston/boston-train.csv', index_col = 'ID', delimiter = ',')
 test = pd.read_csv('../data/Boston/boston-test.csv', index_col = 'ID', delimiter = ',')
 
-y_price = np.log1p(train.medv)
+y_train = np.log1p(train.medv)
 
-dtrain = train.drop(['medv'], axis = 1)
+df_train = train.drop(['medv'], axis = 1)
 
-df_train, df_test, y_train, y_test = train_test_split(dtrain, y_price, test_size = 0.2, shuffle = True)
+#df_train, df_test, y_train, y_test = train_test_split(dtrain, y_price, test_size = 0.2, shuffle = True)
 
-def defaultWithCatVars(df_train, df_test, y_train, y_test):
+def defaultWithCatVars(df_train, y_train, n_folds=5, n_time=3):
 
-    dtrain = pd.get_dummies(df_train, columns= ['chas'])
-    
     sk_boost_clf = Pipeline([('replace_nan', Imputer()), ('to_dense', DenseTransformer()), ('clf', GradientBoostingRegressor())])
+    sk_boost_clf.fit(df_train, y_train)
+    
+    plot_top_features(sk_boost_clf.named_steps['clf'], df_train.columns.values, 20)
     
     time_sum = 0.0
-    for i in range(0,5):
+    for i in range(0,n_time):
         start = time.time()
-        sklearn_cv = cross_val_score(sk_boost_clf, dtrain, y_train, scoring='neg_mean_squared_error', cv=5)
+        sklearn_cv = cross_val_score(sk_boost_clf, df_train, y_train, scoring='neg_mean_squared_error', cv=n_folds, verbose=False)
         end = time.time()
         time_sum += (end-start)
     
-    print('RMSE (GradientBoostingRegressor) = {0}'.format(np.sqrt(-sklearn_cv.mean())))
-    #print ("time: "+ str(end - start))
-    print ("time: " + str(time_sum/5))
+    print('average cv time (GradientBoostingRegressor) = {0:.2f} sec'.format(time_sum / n_time))
+    print('RMSLE (GradientBoostingRegressor) = {0}'.format(np.sqrt(-sklearn_cv.mean())))
 
 
 
@@ -67,55 +67,46 @@ def defaultWithCatVars(df_train, df_test, y_train, y_test):
 #     print ("time: " + str(time_sum/5))
 # =============================================================================
     
-    
 
-### TUNING:
-
-# =============================================================================
-# 
-# def modelfit(alg, dtrain, y_price, predictors, performCV=True, printFeatureImp=True, cv_folds=5):
-#     #Fitting the algorithm on the data
-#     alg.fit(dtrain[predictors], y_price)
-#     
-#     #Predict training set:
-#     dtrain_predictions = alg.predict(dtrain[predictors])
-#     
-#     #Cross Validation
-#     if performCV:
-#         cv_score = cross_val_score(sk_boost_clf, dtrain[predictors], y_price, cv=cv_folds, scoring='neg_mean_squared_error')
-# 
-#     #Report
-#     print ("\nReport")
-#     
-#     
-# =============================================================================
-
-
-
-def tuned(df_train, df_test, y_train, y_test, n_folds=5):
+#Tuning:
+def gridSearch(df_train, y_train, n_folds=5):
     grid_params = {
         'clf__max_depth': [1, 2, 3],
         'clf__learning_rate': [0.1, 0.05, 0.01],
         'clf__n_estimators' : [100, 500, 1000]
     }
-    
     sk_boost_clf = Pipeline([('replace_nan', Imputer()), ('to_dense', DenseTransformer()), ('clf', GradientBoostingRegressor())])
     sk_boost_clf.fit(df_train, y_train)
-    
-    plot_top_features(sk_boost_clf.named_steps['clf'], df_train.columns.values, 20)
     
     grid_search_sk = GridSearchCV(sk_boost_clf, grid_params, scoring='neg_mean_squared_error', cv=n_folds, verbose=True)
     grid_search_sk.fit(df_train, y_train)
     
     best_parameters_sk = max(grid_search_sk.grid_scores_, key=lambda x: x[1])[0]
     
-    print (best_parameters_sk)
+    print ("Best Parameters(sklearn): " + str(best_parameters_sk))
+    tunedWithParams(best_parameters_sk, df_train, y_train)
+    
+    
+    
+def tunedWithParams(best_parameters_sk, df_train, y_train, n_time=3, n_folds=5):
+    sk_boost_clf = Pipeline([('replace_nan', Imputer()),('to_dense', DenseTransformer()), 
+                        ('clf', GradientBoostingRegressor(learning_rate = best_parameters_sk['clf__learning_rate'], 
+                                                 n_estimators = best_parameters_sk['clf__n_estimators'],
+                                                 max_depth = best_parameters_sk['clf__max_depth']))])
+
+    time_sum = 0.0
+    for i in range(0, n_time):
+        t = time.time()
+        sklearn_cv = cross_val_score(sk_boost_clf, df_train, y_train, scoring='neg_mean_squared_error', cv=n_folds, verbose=False)
+        time_sum += time.time() - t
+
+    print('tuned average cv time (GradientBoostingRegressor) = {0:.2f} sec'.format(time_sum / n_time))
+    print('tuned RMSLE (GradientBoostingRegressor) = {0}'.format(np.sqrt(-sklearn_cv.mean())))
 
 
 
 
 #### Method Calls:
 
-#defaultWithCatVars(df_train, df_test, y_train, y_test)
-#defaultWithoutCatVars(df_train, df_test, y_train, y_test)
-tuned(df_train, df_test, y_train, y_test)
+#defaultWithCatVars(df_train, y_train)
+gridSearch(df_train, y_train)
--- project/eda/spyder/boston_xgb.py
+++ project/eda/spyder/boston_xgb.py
@@ -34,27 +34,28 @@ test = pd.read_csv('../data/Boston/boston-test.csv', index_col = 'ID', delimiter
 # xgtest = xgb.DMatrix(df_test.values)
 # =============================================================================
 
-y_price = np.log1p(train.medv)
+y_train = np.log1p(train.medv)
 
-dtrain = train.drop(['medv'], axis = 1)
+df_train = train.drop(['medv'], axis = 1)
 
-df_train, df_test, y_train, y_test = train_test_split(dtrain, y_price, test_size = 0.2, shuffle = True)
+#df_train, df_test, y_train, y_test = train_test_split(dtrain, y_price, test_size = 0.2, shuffle = True)
 
-def defaultWithCatVars(df_train, df_test, y_train, y_test):
+def defaultWithCatVars(df_train, y_train, n_folds=5, n_time=3):
 
     xgboost_clf = Pipeline([('to_dense', DenseTransformer()), ('clf', xgb.XGBRegressor(eval_metric = 'rmse'))])
-
+    xgboost_clf.fit(df_train, y_train)
+    
+    plot_top_features(xgboost_clf.named_steps['clf'], df_train.columns.values, 20)
+    
     time_sum = 0.0
-    for i in range(0,5):
+    for i in range(0,n_time):
         start = time.time()
-        cv = cross_val_score(xgboost_clf, df_train, y_train, scoring='neg_mean_squared_error', cv=5)
+        xgboost_cv = cross_val_score(xgboost_clf, df_train, y_train, scoring='neg_mean_squared_error', cv=n_folds, verbose=False)
         end = time.time()
         time_sum += end-start
-    rmse = np.sqrt(-cv.mean())
 
-    print ('RMSE (XGBRegressor) = {0}'.format(rmse))
-    #print ("time: "+ str(end - start))
-    print ("time: "+ str(time_sum/5))
+    print('average cv time (XGBoost) = {0:.2f} sec'.format(time_sum / n_time))
+    print('RMSLE (XGBoost) = {0}'.format(np.sqrt(-xgboost_cv.mean())))
 
 
 # =============================================================================
@@ -79,28 +80,41 @@ def defaultWithCatVars(df_train, df_test, y_train, y_test):
     
 
 
-def tuned(df_train, df_test, y_train, y_test, n_folds=5):
+def gridSearch(df_train, y_train, n_folds=5):
     grid_params = {
         'clf__max_depth': [1, 2, 3],
         'clf__learning_rate': [0.1, 0.05, 0.01],
         'clf__n_estimators' : [100, 500, 1000]
     }
-    
     xgboost_clf = Pipeline([('to_dense', DenseTransformer()), ('clf', xgb.XGBRegressor(eval_metric = 'rmse'))])
-    xgboost_clf.fit(df_train, y_train)
-    
-    plot_top_features(xgboost_clf.named_steps['clf'], df_train.columns.values, 20)
-    
-    grid_search_xgb = GridSearchCV(xgboost_clf, grid_params, scoring='neg_mean_squared_error', cv=n_folds, verbose=True)
+    grid_search_xgb = GridSearchCV(xgboost_clf, grid_params, scoring='neg_mean_squared_error', cv=n_folds, verbose=False)
     grid_search_xgb.fit(df_train, y_train)
     
     best_parameters_xgb = max(grid_search_xgb.grid_scores_, key=lambda x: x[1])[0]
     
-    print (best_parameters_xgb)
+    print ("Best Parameters(XGBoost): " + str(best_parameters_xgb))
+    tunedWithParams(best_parameters_xgb, df_train, y_train)
+    
+    
+    
+def tunedWithParams(best_parameters_xgb, df_train, y_train, n_time=3, n_folds=5):
+    xgboost_clf = Pipeline([('to_dense', DenseTransformer()), 
+                        ('clf', xgb.XGBRegressor(eval_metric = 'rmse', 
+                                                 learning_rate = best_parameters_xgb['clf__learning_rate'], 
+                                                 n_estimators = best_parameters_xgb['clf__n_estimators'],
+                                                 max_depth = best_parameters_xgb['clf__max_depth']))])
+
+    time_sum = 0.0
+    for i in range(0, n_time):
+        t = time.time()
+        xgboost_cv = cross_val_score(xgboost_clf, df_train, y_train, scoring='neg_mean_squared_error', cv=n_folds, verbose=False)
+        time_sum += time.time() - t
+
+    print('tuned average cv time (XGBoost) = {0:.2f} sec'.format(time_sum / n_time))
+    print('tuned RMSLE (XGBoost) = {0}'.format(np.sqrt(-xgboost_cv.mean())))
 
 
 #### Method Calls:
 
-#defaultWithCatVars(df_train, df_test, y_train, y_test)
-#defaultWithoutCatVars(df_train, df_test, y_train, y_test)
-tuned(df_train, df_test, y_train, y_test)
+#defaultWithCatVars(df_train, y_train)
+gridSearch(df_train, y_train)
