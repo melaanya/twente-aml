7b86716fa5660d6736e6d003e7b9150d7b338c0a
Ashwin Sadananda Bhat <a.sadanandabhat+sourcetree@student.utwente.nl>
2018-01-25 16:23:19 +0100

    tuned code added, result png files added

--- project/eda/spyder/boston_catboost.py
+++ project/eda/spyder/boston_catboost.py
@@ -21,117 +21,77 @@ from sklearn.model_selection import cross_val_score, train_test_split
 import util
 import paramsearch
 from util import plot_top_features
-from util import plot_top_features, catboost_param_tune
+from util import plot_top_features, crossvaltest, catboost_param_tune
 
 
 train = pd.read_csv('../data/Boston/boston-train.csv', index_col = 'ID', delimiter = ',')
 df_test = pd.read_csv('../data/Boston/boston-test.csv', index_col = 'ID', delimiter = ',')
 
-dtrain = train.drop(['medv'], axis=1)
+y_train = np.log1p(train.medv)
 
-y_price = np.log1p(train.medv)
+df_train = train.drop(['medv'], axis=1)
 
-# =============================================================================
-# print (list(df_train))
-# print (list(df_test))
-# print (list(df_train_label))
-# print (y_price)
-# =============================================================================
-
-
-df_train, df_val, y_train, y_val = train_test_split(dtrain, y_price, test_size=0.2)
-
-# =============================================================================
-# def defaultWithoutCatVars(df_train, df_val, y_train, y_val, df_test):
-#     #drop the categorical variables
-#     df_train = df_train.drop(['chas'], axis = 1)
-#     
-#     train_pool = Pool(df_train, y_train)
-#     #val_pool = Pool(X_val, cat_features=[3])
-#     #test_pool = Pool(df_test, cat_features=[3])
-# 
-#     # specify the training parameters 
-#     boston_model = CatBoostRegressor(loss_function = 'RMSE', custom_metric = 'RMSE',  eval_metric = 'RMSE', calc_feature_importance = True)
-#     params = {'loss_function': 'RMSE', 'custom_metric': 'RMSE', 'eval_metric': 'RMSE', 'logging_level': 'Silent'}
-#     
-#     time_sum = 0
-#     for i in range(0,5):
-#         start = time.time()
-#         scores = cv(params, train_pool, fold_count=5)
-#         end = time.time()
-#         time_sum += (end-start)
-#     
-#     #print ('RMSE_train_avg (CatBoostRegressor) = {0}'.format(scores['RMSE_train_avg'][-1]))
-#     print ('RMSE_test_avg (CatBoostRegressor) = {0}'.format(scores['RMSE_test_avg'][-1]))
-#     #print ('RMSE_train_stddev (CatBoostRegressor) = {0}'.format(scores['RMSE_train_stddev'][-1]))
-#     #print ('RMSE_test_stddev (CatBoostRegressor) = {0}'.format(scores['RMSE_test_stddev'][-1]))
-#     #print ("time: "+ str(end - start))
-#     print ("time: "+ str(time_sum/5))
-# =============================================================================
-
-    #result = boston_model.predict(test_pool)
-    #print("Count of trees in model = {}".format(model.tree_count_))
-    #print (result)
-
-#boston_model = CatBoostRegressor(learning_rate = 0.1, loss_function = 'RMSE', custom_metric = 'RMSE',  eval_metric = 'RMSE', calc_feature_importance = True)
-#params = {'iterations':1000, 'depth': 5, 'loss_function': 'RMSE', 'custom_metric': 'RMSE', 'eval_metric': 'RMSE', 'logging_level': 'Verbose'}
-
-# train the model
-#boston_model.fit(train_pool, eval_set= val_pool, use_best_model=True, logging_level='Verbose')
-
-# make the prediction using the resulting model
-#result = boston_model.predict(test_pool)
-#print("Count of trees in model = {}".format(model.tree_count_))
-
-
-#print("RMLSE on val set = %f" % np.sqrt(mean_squared_error(y_val, boston_model.get_test_eval())))
-
-
-def defaultWithCatVars(df_train, df_val, y_train, y_val, df_test):
-    
-    train_pool = Pool(df_train, y_train, cat_features= [3])
+def defaultWithCatVars(df_train, y_train, n_folds=5, n_time=3):
+    cat_indices = [3]
+    train_pool = Pool(df_train, y_train, cat_features=cat_indices)
     # specify the training parameters 
-    boston_model = CatBoostRegressor(loss_function = 'RMSE', custom_metric = 'RMSE',  eval_metric = 'RMSE', calc_feature_importance = True)
     params = {'loss_function': 'RMSE', 'custom_metric': 'RMSE', 'eval_metric': 'RMSE', 'logging_level': 'Verbose'}
+    model = CatBoostRegressor(loss_function = 'RMSE', custom_metric = 'RMSE', eval_metric= 'RMSE', calc_feature_importance = True)
+    model.fit(train_pool, logging_level='Silent')
+    
+    plot_top_features(model, train_pool.get_feature_names(), 20)
     
     time_sum = 0
-    for i in range(0,5):
+    for i in range(0,n_time):
         start = time.time()
-        scores = cv(params, train_pool, fold_count=5)
+        cv_data = cv(params, train_pool, n_folds)
         end = time.time()
         time_sum += (end-start)
     
     
-    #print ('RMSE_train_avg (CatBoostRegressor) = {0}'.format(scores['RMSE_train_avg'][-1]))
-    print ('RMSE_test_avg (CatBoostRegressor) = {0}'.format(scores['RMSE_test_avg'][-1]))
-    #print ('RMSE_train_stddev (CatBoostRegressor) = {0}'.format(scores['RMSE_train_stddev'][-1]))
-    #print ('RMSE_test_stddev (CatBoostRegressor) = {0}'.format(scores['RMSE_test_stddev'][-1]))
-    #print ("time: "+ str(end - start))
-    print ("time: "+ str(time_sum/5))   
-
-
+    print('average cv time (CatBoost) = {0:.2f}'.format(time_sum / n_time))
+    print('RMSLE (CatBoost) = {0}'.format(cv_data['RMSE_test_avg'][-1]))
+    
+    
 #Tuned:
     
-def tuned(df_train, y_train):
-    train_pool = Pool(df_train, label = y_train, cat_features=[3])
+def gridSearch(df_train, y_train):
+    train_pool = Pool(df_train, label = y_train)
     model = CatBoostRegressor(loss_function = 'RMSE', custom_metric = 'RMSE',  calc_feature_importance = True)
     cv_params = model.get_params()
     del cv_params['calc_feature_importance']
     
     model.fit(train_pool, logging_level='Silent')
-    plot_top_features(model, train_pool.get_feature_names(), 20)
     
     cat_grid_params = {
     'depth': [1, 2, 3],
     'learning_rate': [0.1, 0.05, 0.01],
     'iterations' : [100, 500, 1000]
     }
+    
     best_params = catboost_param_tune(cat_grid_params, df_train, y_train, [3], 5)
+    
+    best_params['loss_function'] = 'RMSE'
+    best_params['custom_metric'] = 'RMSE'
+    best_params['calc_feature_importance'] = True
+    print ("Best Parameters (Catboost): " + str(best_params))
+    tunedWithParams(best_params, train_pool, df_train, y_train)
+    
+    
+    
+def tunedWithParams(best_params, train_pool, df_train, y_train, n_time=3, cat_indices=[3], n_folds=5):
+    params = {'loss_function': 'RMSE', 'custom_metric': 'RMSE', 'eval_metric': 'RMSE', 'logging_level': 'Verbose'}
 
+    time_sum = 0.0
+    for i in range(0, n_time):
+        t = time.time()
+        cv_data = cv(params, train_pool, n_folds)
+        time_sum += time.time() - t
 
+    print('tuned average cv time (CatBoost) = {0:.2f}'.format(time_sum / n_time))
+    print('tuned RMSLE (CatBoost) = {0}'.format(cv_data))
 #Method Calls:
 
-defaultWithCatVars(df_train, df_val, y_train, y_val, df_test)
-#defaultWithoutCatVars(df_train, df_val, y_train, y_val, df_test)
-
+#defaultWithCatVars(df_train, y_train)
+gridSearch(df_train, y_train)
 
